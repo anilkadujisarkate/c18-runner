a15884c63852977568aaced02ef2d7693fac98f2
anilkadujisarkate <91275345+anilkadujisarkate@users.noreply.github.com>
2021-10-26 21:00:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+var s1;
+function setup() {
+  createCanvas(400,400);
+  s1 = new Student("anil",14,9);
+  s1.display();
+}
+
+function draw() 
+{
+  background(30)
+
+  
+}
---
+++ student.js
@@ -0,0 +1,19 @@
+
+class Student
+{
+ constructor(name,age,grade)
+ {
+     this.name = name;
+     this.age = age;
+     this.grade = grade;
+ }
+ display()
+ {
+     console.log(this.name);
+    console.log(this.age);
+    console.log(this.grade);
+    }
+}
+
+
+
